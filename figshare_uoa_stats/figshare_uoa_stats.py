73234d43efd6e16aed9ef348b5a7853f1eba54b3
Markus Binsteiner <makkus@gmail.com>
2016-06-28 11:15:46 +1200

    more work on importer

--- figshare_uoa_stats/figshare_uoa_stats.py
+++ figshare_uoa_stats/figshare_uoa_stats.py
@@ -3,14 +3,51 @@ from pigshare.api import figshare_api
 from pigshare.stats_api import figshare_stats_api
 from influxdb import InfluxDBClient
 import pprint
+from pyclist import pyclist
+from pigshare.api import FIGSHARE_BASE_URL
+from pigshare.api import figshare_api
+import os
+import sys
+import ConfigParser
+import argparse
 
-class figshare_stats:
+CONF_FILENAME = 'pigshare.conf'
+CONF_HOME = os.path.expanduser('~/.' + CONF_FILENAME)
 
-    def __init__(self, stats_token, figshare_api_token, institution="auckland", influxdb_host, influxdb_port=8086, influxdb_db="figshare"):
 
-        self.stats_api = figshare_stats_api(institution=institution, stats_token)
+class FigshareStatsConfig(object):
+
+    def __init__(self):
+        self.config = ConfigParser.SafeConfigParser({'token': None, 'url': FIGSHARE_BASE_URL, 'institution': None, 'stats_token': None})
+
+        try:
+            user = os.environ['SUDO_USER']
+            conf_user = os.path.expanduser('~' + user + "/." + CONF_FILENAME)
+            candidates = [conf_user, CONF_HOME]
+        except KeyError:
+            candidates = [CONF_HOME]
+
+        self.config.read(candidates)
+
+        try:
+            self.figshare_url = self.config.get('default', 'url')
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError) as e:
+            self.figshare_url = FIGSHARE_BASE_URL
+
+        try:
+            self.figshare_token = self.config.get('default', 'token')
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError) as e:
+            self.figshare_token = None
+
+
+class figshare_stats_wrapper:
+
+    def __init__(self, stats_token, figshare_api_token, institution="auckland", influxdb_host="metriccollect.cer.auckland.ac.nz", influxdb_port=8086, influxdb_db="figshare"):
+
+        self.stats_api = figshare_stats_api(institution=institution, stats_token=stats_token)
         self.api = figshare_api(token=figshare_api_token)
         self.influxdb = InfluxDBClient(host=influxdb_host, port=influxdb_port, database=influxdb_db)
+        self.articles = None
 
     def get_all_articles(self):
 
@@ -186,29 +223,38 @@ class figshare_stats:
 
     def get_timeline_data(self):
 
-        articles = self.get_all_articles()
+        if not self.articles:
+            self.articles = self.get_all_articles()
 
-        day_stats = self.get_timeline_all_articles(articles)
+        day_stats = self.get_timeline_all_articles(self.articles)
 
         for day,stats in sorted(day_stats.items()):
 
+            if not day:
+                continue
+            print "day: " + str(day)
+            print "stats: " + str(stats)
+
             for article_id, stats_dict in stats['articles'].items():
-                self.insert_into_influxdb(day, 'article', 'total_views', article_id, stats_dict, articles)
-                self.insert_into_influxdb(day, 'article', 'total_downloads', article_id, stats_dict, articles)
+                self.insert_into_influxdb(day, 'article', 'total_views', article_id, stats_dict, self.articles)
+                self.insert_into_influxdb(day, 'article', 'total_downloads', article_id, stats_dict, self.articles)
 
             for author_name, stats_dict in stats['authors'].items():
-                self.insert_into_influxdb(day, 'author', 'total_views', author_name, stats_dict, articles)
-                self.insert_into_influxdb(day, 'author', 'total_downloads', author_name, stats_dict, articles)
+                self.insert_into_influxdb(day, 'author', 'total_views', author_name, stats_dict, self.articles)
+                self.insert_into_influxdb(day, 'author', 'total_downloads', author_name, stats_dict, self.articles)
 
             for category_name, stats_dict in stats['categories'].items():
-                self.insert_into_influxdb(day, 'category', 'total_views', category_name, stats_dict, articles)
-                self.insert_into_influxdb(day, 'category', 'total_downloads', category_name, stats_dict, articles)
+                self.insert_into_influxdb(day, 'category', 'total_views', category_name, stats_dict, self.articles)
+                self.insert_into_influxdb(day, 'category', 'total_downloads', category_name, stats_dict, self.articles)
 
-            self.insert_into_influxdb(day, 'totals', 'total_views', 'total', stats['totals'], articles)
-            self.insert_into_influxdb(day, 'totals', 'total_downloads', 'total', stats['totals'], articles)
+            self.insert_into_influxdb(day, 'totals', 'total_views', 'total', stats['totals'], self.articles)
+            self.insert_into_influxdb(day, 'totals', 'total_downloads', 'total', stats['totals'], self.articles)
+
+            print "XXX"
 
     def insert_into_influxdb(self, date, category, field, item_id, stats_dict, all_articles=None):
 
+        print "1"
 
         json_body_views = {}
 
@@ -221,5 +267,50 @@ class figshare_stats:
         json_body_views['tags'] = {}
         json_body_views['tags']['id'] = item_id
         # json_body_views['tags']['stats_type'] = field
-
+        print "2"
         self.influxdb.write_points([json_body_views])
+
+        print "3"
+
+
+class figshare_stats(object):
+
+    def __init__(self):
+
+        self.config = FigshareStatsConfig()
+
+        self.cli = argparse.ArgumentParser('Convenience wrapper for figshare stats collection')
+        self.cli.add_argument('--token', '-t', help='Token to connect to figshare', default=self.config.figshare_token)
+        self.cli.add_argument('--stats_token', '-s', help='Token to connect to figshare stats api', default=self.config.figshare_token)
+        self.cli.add_argument('--url', '-u', help='Figshare base url', default=self.config.figshare_url)
+        self.cli.add_argument('--profile', '-p', help='Profile to use (profile must be defined in ~/.pigshare.conf), takes precedence over --url and --token config')
+        self.cli.add_argument('--institution', '-i', help='The institution, necessary for some of the stats lookups')
+
+        args = vars(self.cli.parse_args())
+
+        if args.get('profile', None):
+            args['url'] = self.config.config.get(
+                args['profile'], 'url')
+            args['token'] = self.config.config.get(
+                args['profile'], 'token')
+            args['stats_token'] = self.config.config.get(
+                args['profile'], 'stats_token')
+            args['institution'] = self.config.config.get(
+                args['profile'], 'institution')
+
+        self.api = figshare_stats_wrapper(args['stats_token'], args['token'], influxdb_db='figshare_test')
+
+
+
+def run():
+
+    stats = figshare_stats()
+    stats.api.get_timeline_data()
+
+    print "YYYY"
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+   run()
